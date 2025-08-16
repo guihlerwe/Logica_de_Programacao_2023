49cf7b778a91cf585a9661549176fd5406257131
Guilherme <guiihlerwe@icloud.com>
2025-08-16 15:14:45 -0300

    first commmit

---
+++ src/Estrutura_de_Decisao/Basico/Lista1/Exercicio02.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estrutura_de_Decisao.Basico.Lista1;
+
+/**
+ *
+ * @author guilherme
+ */
+public class Exercicio02 {
+    
+}
